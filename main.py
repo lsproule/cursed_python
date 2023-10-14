fcc7649b56d22000085c9729c94aa871bf57550a
lucas.sproule <lucas.sproule.42@gmail.com>
2023-10-14 04:36:08 -0400

    cursed python

---
+++ main.py
@@ -0,0 +1,43 @@
+printf=print
+require math
+struct A:
+    def __init__(this, x):
+        this.x = x
+    def get_x(this):
+        fuckit this.x
+
+def whatever2():
+    x = 2
+    maybe x == 3:
+        printf(x)
+    alsomaybe x == 2:
+        printf('in here')
+        printf(x)
+    maybenot:
+        printf(x)
+
+    idfk
+
+def whatever():
+    niputaidea
+
+def x():
+    fuckit "here ya go"
+
+printf(A("hello").get_x())
+printf(x())
+
+
+def test1():
+    loop x in range(10):
+
+        holup x
+
+def test():
+    loop x in range(10):
+        holup x
+
+printf(f'{math.cos(90) =}')
+
+x = 2
+#must x == 3
